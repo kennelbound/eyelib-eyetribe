5d9cc470509535e28bb1083e93a537d7861d481e
Sam Alston <samalsto@amazon.com>
2015-03-30 15:08:50 -0700

    Moving the logger configuration into the application.

--- src/main/groovy/com/kennelbound/eyelib/eyetribe/EyeTribeEyesManager.groovy
+++ src/main/groovy/com/kennelbound/eyelib/eyetribe/EyeTribeEyesManager.groovy
@@ -2,11 +2,6 @@ package com.kennelbound.eyelib.eyetribe
 
 import com.kennelbound.eyelib.AbstractEyesManager
 import com.kennelbound.eyelib.events.HardwareConnectionStateEvent
-import com.kennelbound.logging.CalibrationCompleteEventLogger
-import com.kennelbound.logging.EyesLocationEventLogger
-import com.kennelbound.logging.HardwareConnectionStateEventLogger
-import com.kennelbound.logging.ViewLocationEventLogger
-
 import com.theeyetribe.client.GazeManager
 import com.theeyetribe.client.IConnectionStateListener
 import com.theeyetribe.client.IGazeListener
@@ -16,27 +11,15 @@ import com.theeyetribe.client.data.GazeData
  * Created by samalsto on 3/23/15.
  */
 class EyeTribeEyesManager extends AbstractEyesManager implements IConnectionStateListener, IGazeListener {
-    @Override
-    void init() {
-        super.init();
-
-        this.addIViewLocationEventListener(new ViewLocationEventLogger());
-        this.addIEyesLocationEventListener(new EyesLocationEventLogger());
-        this.addIHardwareConnectionStateEventListener(new HardwareConnectionStateEventLogger());
-        this.addICalibrationCompleteEventListener(new CalibrationCompleteEventLogger());
-    }
-
     void start() {
         GazeManager.instance.activate(GazeManager.ApiVersion.VERSION_1_0, GazeManager.ClientMode.PUSH, this);
         GazeManager.instance.addGazeListener(this);
     }
 
-    @Override
     void onConnectionStateChanged(boolean isConnected) {
         this.hardwareConnectionStateEvent(isConnected ? HardwareConnectionStateEvent.State.CONNECTED : HardwareConnectionStateEvent.State.NOT_CONNECTED);
     }
 
-    @Override
     void onGazeUpdate(GazeData gazeData) {
         // Send the eye event information
         double distance, leftX, leftY, leftPupilSize, rightX, rightY, rightPupilSize
